1405dc0b0466de8524103bcf2b6a209653682a1d
柒佰拾零 <yan710010@126.com>
2026-01-11 13:23:59 +0800

    feat: 基本实现欢迎语和提醒功能

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,111 @@
+use std::{
+    fs::{File, read_to_string},
+    io::Write,
+};
 
+use anyhow::{Ok, anyhow};
+use indoc::indoc;
+use kdl::KdlDocument;
+
+// 数据结构定义
+#[derive(Default, Debug)]
+pub struct Config {
+    users: Vec<User>,
+}
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub name: String,
+    pub key_fingerprints: Vec<String>,
+    pub no_notify: bool,
+    pub greeting: String,
+}
+
+impl User {
+    pub fn verify(&self, fingerprint: &str) -> bool {
+        self.key_fingerprints.contains(&fingerprint.into())
+    }
+}
+// 默认值
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            name: "UNKNOWN".into(),
+            key_fingerprints: vec![],
+            no_notify: false,
+            greeting: "...你是谁".into(),
+        }
+    }
+}
+
+impl Config {
+    pub fn verify_ssh(&self, fin: &str) -> Option<User> {
+        if fin.is_empty() {
+            return None;
+        }
+        for user in &self.users {
+            if user.verify(fin) {
+                return Some(user.clone());
+            }
+        }
+        None
+    }
+
+    /// 从文件加载用户配置
+    pub fn load() -> anyhow::Result<Config> {
+        // 判断配置文件是否存在
+        let config_path = dirs::config_dir()
+            .ok_or(anyhow!("无法识别配置文件夹"))?
+            .join("ssh-notifier.kdl");
+        if !config_path.try_exists()? {
+            // 配置不存在, 创建默认配置文件并返回
+            eprintln!("创建默认配置文件");
+            let mut file = File::create(config_path)?;
+            file.write_all(indoc! {br#"
+                /- username {
+                    fingerprint "SHA256:xxx..." "SHA256:xxx..."
+                    /- no-notify
+                }
+                "#})?;
+            return Ok(Config::default());
+        }
+        // 读取配置文件并解析
+        let config = read_to_string(config_path)?;
+        let doc: KdlDocument = config.parse()?;
+
+        let mut users: Vec<User> = vec![];
+        for node in doc.nodes() {
+            let name = node.name().value();
+            let Some(children) = node.children() else {
+                // 不儿这都没有那你定义了个什么
+                continue;
+            };
+            let fpr: Vec<String> = children
+                .nodes()
+                .iter()
+                .filter(|n| n.name().value() == "fingerprint")
+                .map(|n| n.entries())
+                .map(|entries| {
+                    entries
+                        .iter()
+                        .map(|e| e.value().as_string().unwrap_or_default().into())
+                })
+                .flatten()
+                .collect();
+            let no_notify = children.get("no-notify").is_some();
+            let greeting = children
+                .get("greeting")
+                .and_then(|n| n.get(0))
+                .and_then(|arg| arg.as_string())
+                .map(|s| s.into())
+                .unwrap_or_default();
+            users.push(User {
+                name: name.into(),
+                key_fingerprints: fpr,
+                no_notify,
+                greeting,
+            });
+        }
+        return Ok(Config { users: users });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 mod config;
 
 use anyhow::Result;
+use anyhow::anyhow;
+use std::path::Path;
 use std::{
     io::{BufRead, BufReader},
     process::{Command, Stdio},
 };
 
+use crate::config::{Config, User};
+
 fn main() -> Result<()> {
     // 使用子进程从journal读取sshd信息
     let mut journal = Command::new("journalctl")
         .args([
-            "--unit", "sshd", "--grep", "Accepted", "--output", "cat", "--since", "now", "--follow",
+            "--unit", "sshd", "--grep", "Accepted", "--output", "short", "--since", "now",
+            "--follow",
         ])
         .stdout(Stdio::piped())
         .stderr(Stdio::null())
@@ -20,6 +25,7 @@ fn main() -> Result<()> {
     let mut reader = BufReader::new(output);
 
     let mut line = String::new();
+    // 主循环
     loop {
         line.clear();
         // 获取
@@ -29,23 +35,23 @@ fn main() -> Result<()> {
             break;
         }
 
-        // 获取需要的部分
+        // 获取连接信息
         let info = SSHInfo::parse(&line);
-        // TODO: 读取配置文件识别密钥指纹
+        // 读取配置文件识别密钥指纹
+        let logined_user = match Config::load() {
+            Ok(conf) => conf.verify_ssh(&info.fingerprint).unwrap_or_default(),
+            Err(err) => {
+                eprintln!("\x1b[31m{}\x1b[0m",err);
+                config::User::default()
+            }
+        };
 
         // 发送有人登录的提示
-        let notify = Command::new("notify-send")
-            .arg("--urgency=critical")
-            .arg("--app-name=ssh-notify")
-            .arg("有人连上来了喵?") // Title
-            .arg(format!(
-                "登录用户:{}\nIP:{}\n指纹:{}",
-                info.user, info.ip, info.fingerprint
-            )) // Message
-            .output();
-        if notify.is_err() || notify.is_ok_and(|o| !o.status.success()) {
-            eprintln!("发送提示失败!");
-        }
+        notify_send(&logined_user, &info);
+        // 显示问候语
+        greet(&logined_user.greeting, &info)
+            .map_err(|e| eprintln!("{}", e))
+            .ok();
     }
     eprintln!("子进程journalctl关闭, 正在退出");
     Ok(())
@@ -54,6 +60,7 @@ fn main() -> Result<()> {
 /// 储存sshd message 的信息
 #[derive(Debug)]
 struct SSHInfo {
+    pub pid: String,
     pub user: String,
     pub ip: String,
     pub fingerprint: String,
@@ -72,14 +79,77 @@ impl SSHInfo {
             }
             None
         };
+        let pid = extract(&msg, "sshd-session[", "]").unwrap_or_default();
         let user = extract(&msg, "for ", " from").unwrap_or("UNKNOWN".into());
         let ip = extract(&msg, "from ", " port").unwrap_or("UNKNOWN".into());
         let fingerprint = msg.split_whitespace().last().unwrap_or("NONE").to_string();
 
         SSHInfo {
+            pid,
             user,
             ip,
             fingerprint,
         }
     }
+}
+
+/// 发送登录提示
+fn notify_send(user: &User, info: &SSHInfo) {
+    if user.no_notify {
+        //return;
+    }
+    // 格式化消息文本
+    let mut title = "有人连上来了喵?";
+    let mut message = format!(
+        "公钥属于 {}\n来自 {}\n登录用户: {}",
+        user.name, info.ip, info.user,
+    );
+    if user.name == "UNKNOWN" {
+        title = "有陌生人连上来了喵！";
+        message.push_str(&format!("\n密钥指纹: {}", info.fingerprint));
+    }
+    let notify = Command::new("notify-send")
+        .arg("--urgency=critical")
+        .arg("--app-name=ssh-notify")
+        .arg(title) // Title
+        .arg(message) // Message
+        .output();
+    if !notify.is_ok_and(|o| o.status.success()) {
+        eprintln!("发送提示失败!");
+    }
+}
+
+fn greet(greeting: &str, info: &SSHInfo) -> Result<()> {
+    // 不需要发送提示的情况
+    if info.pid.is_empty() {
+        return Err(anyhow!("未知的PID, 无法获取用户会话"));
+    }
+    if greeting.is_empty() {
+        return Err(anyhow!("没有需要发送的信息"));
+    }
+    // 从loginctl里面捞tty
+    let output = Command::new("loginctl")
+        .arg("list-sessions")
+        .arg("--no-legend")
+        .output()?;
+    for line in String::from_utf8(output.stdout)?.lines() {
+        // loginctl 输出格式
+        // SESSION UID USER SEAT LEADER CLASS TTY IDLE SINCE
+        let properties: Vec<&str> = line.split_whitespace().collect();
+        if properties.get(4).map(|s| *s == info.pid) == Some(true) {
+            let tty = properties.get(6).ok_or(anyhow!("无法获取tty"))?;
+            // 神奇的ANSI转义环节
+            let greeting_formatted = format!(
+                "{}{}{}{}",
+                "\x1b[s\x1b[4S\x1b[3F", // 保存当前位置 向下滚动(腾出显示空间) 光标上移(达到目标显示位置)
+                "+-----------\n",
+                greeting,
+                "\x1b[u\x1b[4A" // 恢复保存的位置 光标上移(因为之前向下滚动了)
+            );
+            // 注入信息(狠狠灌注!)
+            std::fs::write(Path::new("/dev").join(tty), greeting_formatted)?;
+            return Ok(());
+        }
+    }
+    Err(anyhow!("未找到符合的login"))
 }
